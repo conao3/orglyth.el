51d5fb8527c50d6fba6010cc8553690f95305d00
conao <conao3@gmail.com>
2018-07-28 10:55:02 +0900

    [root] define orglyth group under org

--- orglyth.el
+++ orglyth.el
@@ -24,6 +24,10 @@
 
 ;;; Code:
 
+(defgroup orglyth nil
+  "A orglyth settings org."
+  :group 'org)
+
 (defmacro orglyth-add-list-to-list (dest-lst source-lst &optional append)
   "Add to DEST-LST for SOURCE-LST in a destructive.
 Defaltly, add at the beginning of the list, but when APPEND is non-nil,
